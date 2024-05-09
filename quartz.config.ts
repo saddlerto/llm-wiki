f1ac58480d8868d2c51da5c333e6402167960491
Saddler, Trey (NIH/NIEHS) [C] <trey.saddler@nih.gov>
2024-05-09 16:14:50 -0400

    Quartz sync: May 9, 2024, 4:14 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,16 +8,16 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "GenAI Wiki",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "saddlerto.github.com/genai-wiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
